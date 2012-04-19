00005f77de58b039080cb4e99ea424d6aa0e08a7
Alex Reinisch <areinisc@slu.edu>
2012-04-18 20:17:08 -0500

    tested a variety of cases. works so far

--- README.markdown
+++ README.markdown
@@ -7,3 +7,7 @@ Tree Grafting 2
 ___Objective___
 
 After being given a pre-order traversal of an ordered tree, we report the height of the tree before and after converting it to a rooted binary tree.
+
+___Comments___
+
+It works on a number of test cases. I am pleased.
--- graft2.cpp
+++ graft2.cpp
@@ -6,16 +6,31 @@
  * CONTAINS CODE BORROWED FROM INSTRUCTOR
  */
 #include<iostream>
-#include<algorithm>                             // max function, for convenience 
+#include<algorithm>                                       // max function, for convenience
 using namespace std;
 
-int depth(string::iterator& it) {
-    ++it;                                       // advanced past the conventional 'd' that begins this subtree
-    int answer = 0;
-    while (*it == 'd')                          // we have a child
-        answer = max(answer, 1 + depth(it));    // we are at least one deeper than that child
-    ++it;                                       // advanced past the conventional 'u' that ends this subtree
-    return answer;
+int depth(string::iterator& it, bool converted = false) {
+    if (converted == false) {
+        ++it;                                             // advanced past the conventional 'd' that begins this subtree
+        int answer = 0;
+        while (*it == 'd')                                // we have a child
+            answer = max(answer, 1 + depth(it));          // we are at least one deeper than that child
+        ++it;                                             // advanced past the conventional 'u' that ends this subtree
+        return answer;
+    }
+    else {
+        ++it;                                             // advanced past the conventional 'd' that begins this subtree
+        int answer = 0;
+        while (*it == 'd') 
+            answer = max(answer, 1 + depth(it, true));
+        string::iterator next = it + 1;
+        if (*next == 'd') {
+            ++it;
+            answer = max(answer, 1 + depth(it, true));
+        }
+        ++it;
+        return answer;
+    }
 }
 
 int main() {
@@ -26,7 +41,9 @@ int main() {
         string padded = "d" + original + "u";
         string::iterator start = padded.begin();
         int d = depth(start);
-        cout << "Tree " << treeNum++ << ": " << d << endl;
-        cin << original;
+        start = padded.begin();
+        int dc = depth(start, true);
+        cout << "Tree " << treeNum++ << ": " << d << " => " << dc << endl;
+        cin >> original;
     }
 }
